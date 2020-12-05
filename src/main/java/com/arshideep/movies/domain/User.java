b6921757bc514891d26f8b0e2c2b0bf2e301cba1
harmeet dhot <harmeet2204@gmail.com>
2020-12-05 13:50:48 -0500

    User domain object completed

--- src/main/java/com/arshideep/movies/domain/User.java
+++ src/main/java/com/arshideep/movies/domain/User.java
@@ -3,11 +3,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name="table_user")
 public class User {
+      @Id
+      @GeneratedValue(strategy =GenerationType.AUTO)
+      @Column(name="user_id")
       private Long userId;
+      @Column(length = 255)
       private String firstName;
+      @Column(length = 255)
       private String lastName;
 }
