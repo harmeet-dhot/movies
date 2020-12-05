14de19d09edc3e1d14c8c9cf4b70019832ccabc0
harmeet dhot <harmeet2204@gmail.com>
2020-12-05 13:46:13 -0500

    Added the user domain object

--- src/main/java/com/arshideep/movies/domain/User.java
+++ src/main/java/com/arshideep/movies/domain/User.java
@@ -1,4 +1,13 @@
 package com.arshideep.movies.domain;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+      private Long userId;
+      private String firstName;
+      private String lastName;
 }
